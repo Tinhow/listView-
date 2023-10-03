3b115b189ccf99a479096cd3817108ea7121993e
Walter da Silva Filho <20211370037@ad.ifpb.edu.br>
2023-10-02 21:00:30 -0300

    atividade

---
+++ app/src/main/java/com/example/listgen/new_Pass.kt
@@ -0,0 +1,11 @@
+package com.example.listgen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class new_Pass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_pass)
+    }
+}
